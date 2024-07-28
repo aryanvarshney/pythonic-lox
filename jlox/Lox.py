63f73c2cf47ea2775d37f67f23a4d7995722e4fa
aryanvarshney <aryanv@gmail.com>
2024-07-28 13:26:50 -0700

    Adding basic error handling and Token enums

--- jlox/Lox.py
+++ jlox/Lox.py
@@ -2,6 +2,9 @@ import argparse
 from pathlib import Path
 
 class Lox:
+    def __init__(self):
+        self.hadError = False
+
     def main(self):
         parser = argparse.ArgumentParser(usage="Usage: python Lox.py -f [script]")
         parser.add_argument('-f', type=str, help="Provide a valid file path")
@@ -19,6 +22,9 @@ class Lox:
         with p.open() as f:
             file = f.read()
         self.run(file)
+
+        if self.hadError:
+            SystemExit("There was an error in the code")
     
     def runPrompt(self):
         while True:
@@ -26,10 +32,23 @@ class Lox:
             if line == "exit()":
                 break
             self.run(line)
+            self.hadError = False
         return
     
     def run(self, file):
+        # scanner = Scanner(source)
+        # tokens = scanner.scanTokens()
+
+        # for token in tokens:
+        #     print(token)
         return
+    
+    def error(self, line_number, message):
+        self.report(line_number, "", message)
+    
+    def report(self, line_number, where, message):
+        SystemError("[line " + str(line_number + "] Error" + where + ": " + message))
+        self.hadError = True
 
 
 if __name__ == '__main__':
---
+++ jlox/Token.py
@@ -0,0 +1,60 @@
+from enum import Enum
+
+class TokenType(Enum):
+    # Single-character tokens.
+    LEFT_PAREN = '('
+    RIGHT_PAREN = ')'
+    LEFT_BRACE = '['
+    RIGHT_BRACE = ']'
+    COMMA = ','
+    DOT = '.'
+    MINUS = '-'
+    PLUS = '+'
+    SEMICOLON = ';'
+    SLASH = '/'
+    STAR = '*'
+
+    # One or two character tokens.
+    BANG = '!'
+    BANG_EQUAL = '!='
+    EQUAL = '='
+    EQUAL_EQUAL = '=='
+    GREATER = '>'
+    GREATER_EQUAL = '>='
+    LESS = '<'
+    LESS_EQUAL = '<='
+
+    # Literals.
+    IDENTIFIER = '='
+    STRING = 'String'
+    NUMBER = 'Num'
+
+    # Keywords.
+    AND = 'and'
+    CLASS = 'class'
+    ELSE = 'else'
+    FALSE = 'false'
+    FUN = 'fun'
+    FOR = 'for'
+    IF = 'if'
+    NIL = 'nil'
+    OR = 'or'    
+    PRINT = 'print'
+    RETURN = 'return'
+    SUPER = 'super'
+    THIS = 'this'
+    TRUE = 'true'
+    VAR = 'var'
+    WHILE = 'while'
+
+    EOF = 'EOF'
+
+class Token:
+    def __init__(self, type: TokenType, lexeme: str, literal, line: int):
+        self.type = type
+        self.lexeme = lexeme
+        self.literal = literal
+        self.line = line
+
+    def toString(self):
+        return self.type + " " + self.lexeme + " " + self.literal
